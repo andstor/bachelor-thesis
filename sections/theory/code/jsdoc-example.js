993f855bd661d25c28ffe7e4b08dc1ae9ab98e5c
André Storhaug <andr3.storhaug@gmail.com>
2020-05-06 22:18:09 +0200

    Update theory chapter
    Mainly agile methods section

---
+++ sections/theory/code/jsdoc-example.js
@@ -0,0 +1,8 @@
+/**
+ * Represents a soda bottle.
+ * @constructor
+ * @param {string} brand The brand of the soda.
+ * @param {number} size The size of the soda in deciliters.
+ */
+function SodaBottle(brand, size) {
+}
